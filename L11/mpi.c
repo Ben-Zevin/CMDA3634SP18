57eec9cc25e101a869f80b954774657726b707f3
Ben <benz44@vt.edu>
2018-02-20 16:48:37 -0500

    Changes

--- L11/mpi.c
+++ L11/mpi.c
@@ -5,13 +5,19 @@
 
 int main(int argc, char **argv){
 
+		MPI_Init(&argc, &argv);
+
+		int rank,size;
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		MPI_Comm_size(MPI_COMM_WORLD, &size);
+
 		//need running tallies
 		long long int Ntotal;
 		long long int Ncircle;
 
 		// seed random number generator
-		double seed = 1.0;
-		srand48(seed);
+		//double seed = 1.0;
+		srand48(rank);
 
 		for (long long int n = 0; n < 1000000000; n++){
 				//generate two random numbers
@@ -27,9 +33,18 @@ int main(int argc, char **argv){
 
 
 		double pi = 4.0* Ncircle/ (double) Ntotal;
+		double sum;
+
+		MPI_Allreduce(&Ncircle,
+						&sum,
+						1,
+						MPI_DOULBE,
+						MPI_SUM,
+						MPI_COMM_WORLD);
 
 		printf("Our estimate of pi is %f \n", pi);
 
+		MPI_Finalize();
 		return 0;
 }
 
